de62cd1fb4a7efb016aa779e5dbe084b0dc6020d
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2017-06-01 20:50:42 +0300

    init commit

---
+++ src/main/java/org/zerhusen/am04jwtspringsecuritydemo/Am04JwtSpringSecurityDemoApplication.java
@@ -0,0 +1,12 @@
+package org.zerhusen.am04jwtspringsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Am04JwtSpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Am04JwtSpringSecurityDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/zerhusen/am04jwtspringsecuritydemo/Am04JwtSpringSecurityDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zerhusen.am04jwtspringsecuritydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Am04JwtSpringSecurityDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
